e58849a5c1c95e8d821bd48b0cbdf1e5ceb4b1c3
Nickolay <druumm36@gmail.com>
2025-03-28 12:01:55 +0300

    Добавил весь фронт в папку wwwroot и подержку статических файлов

--- Monitoring.Api/Program.cs
+++ Monitoring.Api/Program.cs
@@ -20,6 +20,7 @@ var app = builder.Build();
 QuestPDF.Settings.License = LicenseType.Community;
 
 // Настраиваем Middleware Pipeline
+app.UseStaticFiles();
 app.UseSwaggerDocumentation();  // Подключаем Swagger
 app.UseRouting();
 app.UseCors("MyPolicy");
